0167946ad6ae355150c1fce4c0402fa75d4924d7
shane.daly <shane.daly@wpengine.com>
2018-02-05 16:52:40 +0000

    merge in remote

--- app/components/AreaChart/index.js
+++ app/components/AreaChart/index.js
@@ -1,35 +1,46 @@
 import React, { PropTypes } from 'react';
-import { Area, AreaChart as ReAreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+  Area,
+  AreaChart as ReAreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis } from 'recharts';
 
 const AreaChart = (props) => (
-  <ReAreaChart
-    width={730}
-    height={250}
-    data={props.data}
-    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-  >
-    <defs>
-      <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-        <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-      </linearGradient>
-      <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-      </linearGradient>
-    </defs>
-    <XAxis dataKey="name" />
-    <YAxis />
-    <CartesianGrid strokeDasharray="3 3" />
-    <Tooltip />
-    <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-    <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-  </ReAreaChart>
+  <ResponsiveContainer height={props.height}>
+    <ReAreaChart
+      data={props.data}
+    >
+      <defs>
+        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+          <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+        </linearGradient>
+        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+          <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+        </linearGradient>
+      </defs>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Tooltip />
+      <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+      <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+    </ReAreaChart>
+  </ResponsiveContainer>
   );
 
 
 AreaChart.propTypes = {
   data: PropTypes.array,
+  height: PropTypes.number,
+};
+
+AreaChart.defaultProps = {
+  height: 250,
 };
 
 export default AreaChart;
