e54a3cb52f30fa68eda97bb79f45869905ff318a
Dave Cremins <dave.cremins@wpengine.com>
2018-02-05 21:30:01 +0000

    Refactor to use existing middleware structure

--- server/index.js
+++ server/index.js
@@ -9,12 +9,9 @@ const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
-const setupSocket = require('./middlewares/socketMiddleware');
-const setupWebhooks = require('./hooks/hooks');
+const setupSocketMiddleware = require('./middlewares/socketMiddleware');
+const setupWebhooksMiddleware = require('./middlewares/webhooksMiddleware');
 const app = express();
-
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
@@ -47,9 +44,5 @@ const server = app.listen(port, host, (err) => {
   }
 });
 
-const socketInterface = setupSocket(server);
-setInterval(() => {
-  socketInterface.emit('area:changeDataForTest');
-}, 1500);
-
-setupWebhooks(express, app, socketInterface);
+const socketInterface = setupSocketMiddleware(server, {isDev: isDev, devInterval: 2000});
+setupWebhooksMiddleware(app, socketInterface);
--- server/middlewares/socketMiddleware.js
+++ server/middlewares/socketMiddleware.js
@@ -3,12 +3,16 @@
 /**
  * Socket middleware
  */
-module.exports = (server) => {
+module.exports = (server, opts) => {
    const io = require('socket.io')(server);
 
    io.on('connection', (socket) => {
       socket.emit('connectionMessage', {msg: 'Socket connection successfully established'});
    });
+
+   if(opts.isDev){
+    setInterval(() => { io.emit('area:changeDataForTest'); }, opts.devInterval);
+   }
 
    return io;
  };
