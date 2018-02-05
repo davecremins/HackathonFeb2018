ac6ccdc6502042e6cffd722ca056d87ee06e58ce
shane.daly <shane.daly@wpengine.com>
2018-02-05 15:03:11 +0000

    add chart to home page

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -17,6 +17,17 @@ import AreaChart from '../components/AreaChart';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const data = [
+      { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+      { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+      { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+      { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+      { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+      { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+      { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+      { name: 'Page G', uv: 7490, pv: 600, amt: 2600 },
+    ];
+
     return (
       <Box>
         <h1>
@@ -24,7 +35,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
         </h1>
         <Box w={1 / 3}>sdasdasd</Box>
 
-        <AreaChart />
+        <AreaChart data={data} />
       </Box>
     );
   }
